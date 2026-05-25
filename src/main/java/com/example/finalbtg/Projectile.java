8fc7c69045919595dc0fcc477400fea229aaf7a2
anik-tB <mdabusayumanik123@gmail.com>
2026-05-25 20:01:34 +0600

    refactor: Upgrade Projectile move system to support multi-directional spells

--- src/main/java/com/example/finalbtg/Projectile.java
+++ src/main/java/com/example/finalbtg/Projectile.java
@@ -117,151 +117,67 @@ public class Projectile {
 
 
     public void move() {
-        if (Id == 1) {  // Player projectile
-            switch (direction) {
-                case "UP":
-                    posY -= speed;
-                    break;
-                case "DOWN":
-                    posY += speed;
-                    break;
-                case "LEFT":
-                    posX -= speed;
-                    break;
-                case "RIGHT":
-                    posX += speed;
-                    break;
-            }
-        }
-        else if(Id==2){
-
-            switch (direction) {
-                case "UP":
-                    posY -= speed;
-                    break;
-                case "DOWN":
-                    posY += speed;
-                    break;
-                case "LEFT":
-                    posX -= speed;
-                    break;
-                case "RIGHT":
-                    posX += speed;
-                    break;
-                case "UP_RIGHT":
-                    posY -= speed * 0.7071;
-                    posX += speed * 0.7071;
-                    break;
-                case "UP_LEFT":
-                    posY -= speed * 0.7071;
-                    posX -= speed * 0.7071;
-                    break;
-                case "DOWN_LEFT":
-                    posY += speed * 0.7071;
-                    posX -= speed * 0.7071;
-                    break;
-                case "DOWN_RIGHT":
-                    posY += speed * 0.7071;
-                    posX += speed * 0.7071;
-                    break;
-                case "UP_LEFT_LEFT":
-                    posY -= speed * 0.3827; // Approx. sin(22.5)
-                    posX -= speed * 0.9239; // Approx. cos(22.5)
-                    break;
-                case "UP_LEFT_UP":
-                    posY -= speed * 0.9239;
-                    posX -= speed * 0.3827;
-                    break;
-                case "UP_RIGHT_UP":
-                    posY -= speed * 0.9239;
-                    posX += speed * 0.3827;
-                    break;
-                case "UP_RIGHT_RIGHT":
-                    posY -= speed * 0.3827;
-                    posX += speed * 0.9239;
-                    break;
-                case "DOWN_RIGHT_RIGHT":
-                    posY += speed * 0.3827;
-                    posX += speed * 0.9239;
-                    break;
-                case "DOWN_RIGHT_DOWN":
-                    posY += speed * 0.9239;
-                    posX += speed * 0.3827;
-                    break;
-                case "DOWN_LEFT_DOWN":
-                    posY += speed * 0.9239;
-                    posX -= speed * 0.3827;
-                    break;
-                case "DOWN_LEFT_LEFT":
-                    posY += speed * 0.3827;
-                    posX -= speed * 0.9239;
-                    break;
-            }
-        }
-        else {
-            // Calculate diagonal movement based on direction
-            switch (direction) {
-                case "UP":
-                    posY -= speed;
-                    break;
-                case "DOWN":
-                    posY += speed;
-                    break;
-                case "LEFT":
-                    posX -= speed;
-                    break;
-                case "RIGHT":
-                    posX += speed;
-                    break;
-                case "UP_RIGHT":
-                    posY -= speed * 0.7071;
-                    posX += speed * 0.7071;
-                    break;
-                case "UP_LEFT":
-                    posY -= speed * 0.7071;
-                    posX -= speed * 0.7071;
-                    break;
-                case "DOWN_LEFT":
-                    posY += speed * 0.7071;
-                    posX -= speed * 0.7071;
-                    break;
-                case "DOWN_RIGHT":
-                    posY += speed * 0.7071;
-                    posX += speed * 0.7071;
-                    break;
-                case "UP_LEFT_LEFT":
-                    posY -= speed * 0.3827; // Approx. sin(22.5)
-                    posX -= speed * 0.9239; // Approx. cos(22.5)
-                    break;
-                case "UP_LEFT_UP":
-                    posY -= speed * 0.9239;
-                    posX -= speed * 0.3827;
-                    break;
-                case "UP_RIGHT_UP":
-                    posY -= speed * 0.9239;
-                    posX += speed * 0.3827;
-                    break;
-                case "UP_RIGHT_RIGHT":
-                    posY -= speed * 0.3827;
-                    posX += speed * 0.9239;
-                    break;
-                case "DOWN_RIGHT_RIGHT":
-                    posY += speed * 0.3827;
-                    posX += speed * 0.9239;
-                    break;
-                case "DOWN_RIGHT_DOWN":
-                    posY += speed * 0.9239;
-                    posX += speed * 0.3827;
-                    break;
-                case "DOWN_LEFT_DOWN":
-                    posY += speed * 0.9239;
-                    posX -= speed * 0.3827;
-                    break;
-                case "DOWN_LEFT_LEFT":
-                    posY += speed * 0.3827;
-                    posX -= speed * 0.9239;
-                    break;
-            }
+        switch (direction) {
+            case "UP":
+                posY -= speed;
+                break;
+            case "DOWN":
+                posY += speed;
+                break;
+            case "LEFT":
+                posX -= speed;
+                break;
+            case "RIGHT":
+                posX += speed;
+                break;
+            case "UP_RIGHT":
+                posY -= speed * 0.7071;
+                posX += speed * 0.7071;
+                break;
+            case "UP_LEFT":
+                posY -= speed * 0.7071;
+                posX -= speed * 0.7071;
+                break;
+            case "DOWN_LEFT":
+                posY += speed * 0.7071;
+                posX -= speed * 0.7071;
+                break;
+            case "DOWN_RIGHT":
+                posY += speed * 0.7071;
+                posX += speed * 0.7071;
+                break;
+            case "UP_LEFT_LEFT":
+                posY -= speed * 0.3827; // Approx. sin(22.5)
+                posX -= speed * 0.9239; // Approx. cos(22.5)
+                break;
+            case "UP_LEFT_UP":
+                posY -= speed * 0.9239;
+                posX -= speed * 0.3827;
+                break;
+            case "UP_RIGHT_UP":
+                posY -= speed * 0.9239;
+                posX += speed * 0.3827;
+                break;
+            case "UP_RIGHT_RIGHT":
+                posY -= speed * 0.3827;
+                posX += speed * 0.9239;
+                break;
+            case "DOWN_RIGHT_RIGHT":
+                posY += speed * 0.3827;
+                posX += speed * 0.9239;
+                break;
+            case "DOWN_RIGHT_DOWN":
+                posY += speed * 0.9239;
+                posX += speed * 0.3827;
+                break;
+            case "DOWN_LEFT_DOWN":
+                posY += speed * 0.9239;
+                posX -= speed * 0.3827;
+                break;
+            case "DOWN_LEFT_LEFT":
+                posY += speed * 0.3827;
+                posX -= speed * 0.9239;
+                break;
         }
         updatePosition();
     }
